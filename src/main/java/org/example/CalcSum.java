b52a543b07a8e824d09c7f2910ddefb1e0ae3f5b
abalmasDA <abalmasit@gmail.com>
2023-07-26 15:17:45 +0300

    Commit1

--- src/main/java/org/example/CalcSum.java
+++ src/main/java/org/example/CalcSum.java
@@ -1,2 +1,12 @@
-package org.example;public class CalcSum {
+package org.example;
+
+import java.util.ArrayList;
+
+public class CalcSum {
+    public  int calcSum(ArrayList<Integer> someListInteger) {
+        return someListInteger.stream().map(num -> num * num).reduce(0, Integer::sum);
+
+    }
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,34 @@
-package org.example;public class Main {
+/**
+ * Створити список та заповнити рандомними числами 10 осередків, потрібно вивести суму квадратів усіх елементів списку.
+ * Ми використовуємо метод map() для зведення квадрат кожного елемента, а потім застосовуємо метод reduce() для обʼєднання всіх елементів в одне число.
+ */
+
+
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numberElements = 10;
+        ArrayList<Integer> someList = fillingList(numberElements);
+
+        CalcSum calcSum = new CalcSum();
+        System.out.println(calcSum.calcSum(someList));
+
+
     }
+
+
+    public static ArrayList<Integer> fillingList(int numberElements) {
+        Random random = new Random();
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < numberElements; i++) {
+            result.add(random.nextInt(100) + 1);
+        }
+        return result;
+    }
+
+
 }
